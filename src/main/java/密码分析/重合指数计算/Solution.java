0ada777843eaf56dd555e54b066389423e9e017a
githubtmy <734222381@qq.com>
2021-12-03 23:11:14 +0800

    add 更新重合指数计算

--- src/main/java/密码分析/重合指数计算/Solution.java
+++ src/main/java/密码分析/重合指数计算/Solution.java
@@ -2,23 +2,72 @@ package 密码分析.重合指数计算;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Solution {
 
     public static void main(String[] args) {
-        String ciphertext = "FSHF PMGH TFVM AZPP ZYUB MMGZ SOVI NFUM" +
-                "KCZM ZSOM GZVN FFWK IVAH YZAZ SOGF KTUY" +
-                "GTIM GHTI MZYI BNIY WOCF USAM FZSY BUAH" +
-                "YCDL MFOC ILGD ZVIN CFIA VUNQ HYMI SAZM" +
-                "CHRU ZCHV WSFK BHAO HFPV HEHC IBIC HIVF" +
-                "PTIM GHTI MZYV ZSYB UAZS OSUT NHCM GHFC" +
-                "DOCF ULVC ZSOV ISAP ZHBA VBZS HICI BOHN" +
-                "CILC FNIN ZBZM DISA ZSPF CTIM ZFSM GHFC";
-        calIndOfCoincidence(ciphertext);
+        String rawtext =
+                        "CHRE EVOA HMAE RATB IAXX WTNX BEEO PHBS BQMQ EQER BWRV " +
+                        "XUOA KXAO SXXW EAHB WGJM MQMN KGRF VGXW TRZX WIAK LXFP " +
+                        "SKAU TEMN DCMG TSXM XBTU IADN GMGP SREL XNJE LXVR VPRT " +
+                        "ULHD NQWT WDTY GBPH XTFA LJHA SVBF XNGL LCHR ZBWE LEKM " +
+                        "SJIK NBHW RJGN MGJS GLXF EYPH AGNR BIEQ JTAM RVLC RREM " +
+                        "NDGL XRRI MGNS NRWC HRQH AEYE VTAQ EBBI PEEW EVKA KOEW " +
+                        "ADRE MXMT BHHC HRTK DNVR ZCHR CLQO HPWQ AIIW XNRM GWOI " +
+                        "IFKE E";
+        System.out.println("请输入m: ");
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println("---------------------------------------");
+        String ciphertext = pretreatment(rawtext);
+        System.out.println("ciphertext = " + ciphertext);
+        System.out.println("ciphertext's length = " + ciphertext.length());
+        System.out.println("---------------------------------------");
+        mCals(m, ciphertext);
+        System.out.println("---------------------------------------");
+
+    }
+
+    /**
+     * 预处理：去除字符串的空格
+     * @param rawtext
+     * @return
+     */
+    public static String pretreatment(String rawtext) {
+        String ciphertext = "";
+        for (int i = 0; i < rawtext.length(); i++) {
+            if (rawtext.charAt(i) == ' ') {
+                continue;
+            }
+            ciphertext = ciphertext + rawtext.charAt(i);
+        }
+        return ciphertext;
+    }
+
+    /**
+     * 对输入的m，分别做m次重合指数计算
+     * @param ciphertext
+     */
+    public static void mCals(int m, String ciphertext) {
+        for (int i = 0; i < m; i++) {
+            String str = "";
+            System.out.println("lc(y" + (i + 1) + ")");
+            for (int j = 0; j < ciphertext.length() / m; j++) {
+                str = str + ciphertext.charAt(m * j + i);
+            }
+            if (i < (ciphertext.length() % m)) {
+                str = str + ciphertext.charAt(m * (ciphertext.length() / m) + i);
+            }
+            String ciphertextSegment = str;
+            System.out.println("y" + (i + 1) + " : ciphertextSegment = " + str);
+            calIndOfCoincidence(ciphertextSegment);
+        }
     }
 
     /**
      * 计算重合指数
+     *
      * @param ciphertext
      */
     public static void calIndOfCoincidence(String ciphertext) {
@@ -30,19 +79,21 @@ public class Solution {
         }
         System.out.println("Σfi(fi - 1) = " + sum);
         int count = 0;
-        for(int i = 0; i < ciphertext.length(); i++) {
-            if(ciphertext.charAt(i) != ' ') {
+        for (int i = 0; i < ciphertext.length(); i++) {
+            if (ciphertext.charAt(i) != ' ') {
                 count++;
             }
         }
         double n = count * (count - 1);
         System.out.println("n(n - 1) = " + n);
         double lc = sum / n;
-        System.out.println("重合指数lc(X) = " + lc );
+        System.out.println("重合指数 = " + lc);
+        System.out.println("---------------------------------------");
     }
 
     /**
      * 统计字母频数
+     *
      * @param cipherText
      * @return
      */
