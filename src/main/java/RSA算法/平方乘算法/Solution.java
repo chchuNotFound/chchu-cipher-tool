b45b416cc6eb4c3909bf328964e7d848e32f768c
githubtmy <734222381@qq.com>
2021-12-20 20:09:43 +0800

    fix 平方乘算法中的bug

--- src/main/java/RSA算法/平方乘算法/Solution.java
+++ src/main/java/RSA算法/平方乘算法/Solution.java
@@ -1,32 +1,34 @@
 package RSA算法.平方乘算法;
 
-import static java.lang.StrictMath.floorMod;
-
 public class Solution {
     public static void main(String[] args) {
         int x = 3;
         int c = 9;
         int n = 19684;
-        squareMultiply(x, c, n);
+        System.out.println(squareMultiply(x, c, n));
     }
+
 
     /**
      * 平方-乘算法
-     * @param x 底数
-     * @param c 指数
-     * @param n 模数
+     * @param x
+     * @param c
+     * @param n
+     * @return
      */
-    public static void squareMultiply(int x, int c, int n) {
+    public static long squareMultiply(long x, int c, long n) {
         String ci = Integer.toBinaryString(c);
         int count = ci.length();
         System.out.println("指数c 的 二进制表示 ： " + ci);
-        int z = 1;
-        for(int i = count - 1; i >= 0; i--) {
-            z = floorMod(z * z, n);
+        long z = 1;
+        // i从字符串的低位（从二进制码的高位开始）
+        for(int i = 0; i < count; i++) {
+            z = Math.floorMod(z * z, n);
             if(ci.charAt(i) == '1') {
-                z = floorMod( z * x, n);
+                z = Math.floorMod(z * x, n);
             }
+            System.out.println("第" + (count - i - 1) + "个比特为"+ ci.charAt(i) + "，得: z = " + z);
         }
-        System.out.println("余数z:" + z);
+        return z;
     }
 }
--- src/main/java/数论/本原元素证明/Solution.java
+++ src/main/java/数论/本原元素证明/Solution.java
@@ -5,18 +5,52 @@ import static java.lang.Math.floorMod;
 public class Solution {
 
     public static void main(String[] args) {
-        // 阶的计算fi(n) = n - 1
-        proofPriElement(2, 11);
+        // 模数若是素数p，阶的计算Ord(a) = p - 1
+        // 如底数为7，指数为n（从1开始计数）,模数为7919 ，阶值为7918
+        proofPriElement(7, 7919);
     }
+
+    /**
+     * 本原元素证明
+     * @param element
+     * @param modNum
+     */
     public static void proofPriElement(int element, int modNum) {
-        int ans = 0;
+        long ans = 0;
         int count = 1;
-        while( ans != 1) {
-            int pow = (int) Math.pow(element, count);
-            ans = floorMod(pow, modNum);
+        while(ans != 1) {
+            ans = squareMultiply(element, count, modNum);
             System.out.println("第" + count + "次：" + ans);
             count++;
+            if(count == modNum) {
+                break;
+            }
+        }
+        if(count == modNum) {
+            System.out.println( element + "是模"+ modNum + "的一个本原元素。");
         }
         return;
+    }
+
+    /**
+     * 平方-乘算法
+     * @param x 底数
+     * @param c 指数
+     * @param n 模数
+     */
+    public static long squareMultiply(long x, int c, long n) {
+        String ci = Integer.toBinaryString(c);
+        int count = ci.length();
+//        System.out.println("指数c 的 二进制表示 ： " + ci);
+        long z = 1;
+        // i从字符串的低位（从二进制码的高位开始）
+        for(int i = 0; i < count; i++) {
+            z = floorMod(z * z, n);
+            if(ci.charAt(i) == '1') {
+                z = floorMod(z * x, n);
+            }
+//            System.out.println("第" + (count - i - 1) + "个比特为"+ ci.charAt(i) + "，得: z = " + z);
+        }
+        return z;
     }
 }
